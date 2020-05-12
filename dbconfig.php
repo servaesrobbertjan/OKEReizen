5075d2dd6e6ddbe2fb721672afde320e592a8101
OlivierJanssen <60945025+OlivierJanssen@users.noreply.github.com>
2020-05-12 19:27:42 +0200

    Delete dbconfig.php

--- dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class DBConfig
-{
-    public static $DB_CONNSTRING = "mysql:host=localhost;port=3306;dbname=okereizen;charset=utf8";
-    public static $DB_USER = "root";
-    public static $DB_PASSWORD = "";
-}
