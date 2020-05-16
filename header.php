10aa9350a4405da4b21c3bbbaec5289be66d602f
Evi Vandael <evivandael@gmail.com>
2020-05-16 10:49:27 +0200

    enkele aanpassingen gedaan

--- header.php
+++ header.php
@@ -12,11 +12,7 @@
     <a href="review.php">Reviews</a> -
     <?php if (!isset($_SESSION["gebruiker"])) { ?>
         <a href="login.php">Login</a> -
-<<<<<<< HEAD
         <a href="registreer.php">Registreren</a> -
-=======
-        <a href="registreer.php">Registreren </a>
->>>>>>> 5a3a953147583a023c9d6c13c4cbf9e51c7f137b
     <?php } else { ?>
         <a href="logout.php">Logout</a> -
         <a href="privatepage.php">Privé-pagina</a>
