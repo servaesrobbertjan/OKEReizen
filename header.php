9317c4820090d232a8bf059c352cb901f64dbcda
Kristof Mentens <kmjmentens@gmail.com>
2020-06-02 14:25:00 +0200

    aanpassing header voor admin - kme

--- header.php
+++ header.php
@@ -1,3 +1,8 @@
+<?php
+
+require_once("klanten.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,16 +22,26 @@
             <a href="allepakketten.php"> Onze Pakketten </a>|
             <a href="hoteldetail.php"> Onze Hotels </a>|
             <a href="reviewPage.php"> Reviews </a>|
-            <?php if (!isset($_SESSION["gebruiker"])) { ?>
+            <?php if (!isset($_SESSION["gebruiker"])) {
+                ?>
                 <a href="login.php"> Login </a>|
                 <a href="registreer.php"> Registreren </a>
-            <?php } else { ?>
+            <?php } else { 
+                if (isset($_SESSION["gebruiker"])) {
+
+                    $gebruiker = unserialize($_SESSION["gebruiker"]);
+                    $naam = $gebruiker->getNaam();
+                }
+                ?>
                 <a href="logout.php"> Logout </a>|
                 <a href="klantPage.php"> Mijn OKERreizen </a>
+                <?php if ($naam=="Admin") { ?>
+            <td><a href="CSV.php" target="_blank">| Administrator</a></td>
             <?php
             }
+        }
             ?>
-            <td><a href="csv.pdf" target="_blank"></a></td>
+           
         </div>
     </div>
     <div class=wrapper>
